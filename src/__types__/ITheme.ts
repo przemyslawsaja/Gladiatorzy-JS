0022f00bb3017ab0a8fce4249929fee445fbd657
unknown <przemyslawsaja@gmail.com>
2020-11-10 20:32:24 +0100

    added theme

---
+++ src/__types__/ITheme.ts
@@ -0,0 +1,17 @@
+export interface ITheme {
+  primmary: string,
+  secondary: string,
+  grey1: string,
+  grey2: string,
+  grey3: string,
+  grey4: string,
+  grey5: string,
+  grey6: string,
+  fontSize: {
+    xs: string, 
+    s: string,
+    m: string,
+    l: string,
+    xl: string,
+  },
+ }
---
+++ src/theme/mainTheme.ts
@@ -0,0 +1,19 @@
+import { ITheme } from '../__types__/ITheme'
+
+export const theme:ITheme = {
+  primmary: '#F8DF2A',
+  secondary: '#34D9EA',
+  grey1: '#1B2029',
+  grey2: '#505869',
+  grey3: '#868D9C',
+  grey4: '#B0B7C5',
+  grey5: '#E4E6EA',
+  grey6: '#FFFFFF',
+  fontSize: {
+      xs: '0.875em',
+      s: '1em',
+      m: '1.375em',
+      l: '2.125em',
+      xl: '2.75em',
+  },
+};
