a7ed4e2dc9a201eaf422dd7c4ab814aa6ef329e4
unknown <przemyslawsaja@gmail.com>
2020-11-11 00:44:34 +0100

    Update icons

--- src/assets/icons/HamburgerIcon.tsx
+++ src/assets/icons/HamburgerIcon.tsx
@@ -5,7 +5,7 @@ const Svg = styled.svg`
   width: 20px; 
   height: auto; 
 `
-const Hamburger = () => {
+const HamburgerIcon = () => {
   return (
     <Svg 
       width="18" 
@@ -18,10 +18,9 @@ const Hamburger = () => {
         d="M0 12H18V10H0V12ZM0 7H18V5H0V7ZM0 0V2H18V0H0Z" 
         fill="black"
       />
-
     </Svg>
 
   )
 }
 
-export default Hamburger;
+export default HamburgerIcon;
