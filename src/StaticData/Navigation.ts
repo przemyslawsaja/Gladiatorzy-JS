0741fa063a38da3d9c5c1f177bad3f4bcfed30e5
unknown <przemyslawsaja@gmail.com>
2020-12-23 01:04:56 +0100

    update navlinks

--- src/StaticData/Navigation.ts
+++ src/StaticData/Navigation.ts
@@ -1,7 +1,7 @@
 const NavigationLinks = [
   {path: '/',  content: 'Strona główna'},
-  {path: '/about',  content: 'O mnie'},
-  {path: '/blog', content: 'Blog'},
-  {path: '/contact',  content: 'Kontakt'},
+  {path: '/about',  content: 'Autorzy'},
+  {path: '/curses', content: 'Wszystkie kursy'},
+  {path: '/profile', content: 'Moje kursy'},
 ]
 export default NavigationLinks;
